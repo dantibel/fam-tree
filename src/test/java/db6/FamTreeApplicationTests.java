c90d7bed17810d46f32a1c37424d3124bc119576
Danil Belov <danil.andr.belov@gmail.com>
2025-03-15 13:57:19 +0200

    Create initial Spring project structure

---
+++ src/test/java/db6/FamTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package db6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
