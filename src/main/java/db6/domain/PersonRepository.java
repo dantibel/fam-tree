70dca79f580118c5ca4adfe4923a301062905587
Danil Belov <danil.andr.belov@gmail.com>
2025-03-25 15:06:26 +0200

    Add initial CRUD repositories for entries

---
+++ src/main/java/db6/domain/PersonRepository.java
@@ -0,0 +1,8 @@
+package db6.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
---
+++ src/main/java/db6/domain/RelationRepository.java
@@ -0,0 +1,9 @@
+package db6.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RelationRepository extends JpaRepository<Relation, Long> {
+}
+
